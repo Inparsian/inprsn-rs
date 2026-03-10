c0c857e305b71b5d997319c38b45239bc7bcf814
Inparsian <im@inpr.sn>
2026-03-10 14:52:10 -0400

    feat(taskmanager): make look pretty

--- src/apps/taskmanager.rs
+++ src/apps/taskmanager.rs
@@ -9,7 +9,7 @@ pub fn new_taskmanager_instance() -> Process {
     let pid = process.id;
     process.add_window(WindowInstance::new(WindowInstanceProps {
         title: "task manager".to_owned(),
-        size: ScreenCoordinates::Absolute { x: 332, y: 396 },
+        size: ScreenCoordinates::Absolute { x: 332, y: 250 },
         on_close: Some(Rc::new(move |_| {
             os::kill_process(pid);
         })),
@@ -25,26 +25,27 @@ pub fn new_taskmanager_instance() -> Process {
 fn WindowTaskManager() -> Element {
     rsx! {
         div {
-            class: "taskmanager-header",
-            span { "id" },
-            span { "name" },
-            span { "actions" },
-        }
-        div {
-            class: "taskmanager-list",
-            for process in PROCESSES.read().iter() {
-                div {
-                    class: "taskmanager-item",
-                    
-                    span { {process.id.to_string()} }
-                    span { {process.name.clone()} }
-                    div {
-                        a {
-                            onclick: {
-                                let pid = process.id;
-                                move |_| os::kill_process(pid)
-                            },
-                            "kill"
+            class: "taskmanager",
+            
+            table {
+                tr {
+                    th { "id" }
+                    th { "name" }
+                    th { "actions" }
+                }
+                
+                for process in PROCESSES.read().iter() {
+                    tr {
+                        th { {process.id.to_string()} }
+                        th { {process.name.clone()} }
+                        th {
+                            a {
+                                onclick: {
+                                    let pid = process.id;
+                                    move |_| os::kill_process(pid)
+                                },
+                                "kill"
+                            }
                         }
                     }
                 }
