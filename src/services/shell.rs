df4244aa54fb02de3d6f1ff18b407a61bce242ba
Inparsian <im@inpr.sn>
2026-03-20 15:35:20 -0400

    feat(terminal): handle arrow keys

--- src/services/shell.rs
+++ src/services/shell.rs
@@ -3,6 +3,7 @@ use crate::consts::{ANSI_CLEAR_SCREEN, ANSI_CURSOR_HOME, ANSI_RESET, ANSI_RED};
 pub struct Shell {
     pub pwd: String,
     pub buffer: String,
+    pub pos: usize,
     pub tx: async_channel::Sender<String>,
     pub rx: async_channel::Receiver<String>,
 }
@@ -13,6 +14,7 @@ impl Default for Shell {
         let sh = Self {
             pwd: "/home/inparsian".to_owned(),
             buffer: String::new(),
+            pos: 0,
             tx,
             rx,
         };
@@ -33,12 +35,37 @@ impl Shell {
         format!("{path} {ANSI_RED}›{ANSI_RESET} ")
     }
     
+    pub fn move_cursor(&mut self, delta: i32) -> String {
+        let new_pos = (self.pos as i32 + delta).clamp(0, self.buffer.len() as i32) as usize;
+        if new_pos != self.pos {
+            let out = if delta > 0 {
+                "\x1B[C"
+            } else {
+                "\x1B[D"
+            };
+            self.pos = new_pos;
+            out.to_owned()
+        } else {
+            String::new()
+        }
+    }
+    
     pub fn handle_stdin(&mut self, key: &str) {
         match key {
+            "\x1B[A" | "\x1B[B" => {}, // up | down
+            "\x1B[C" => { // right
+                let res = self.move_cursor(1);
+                let _ = self.tx.try_send(res);
+            },
+            "\x1B[D" => { // left
+                let res = self.move_cursor(-1);
+                let _ = self.tx.try_send(res);
+            },
             "\r" => {
                 let _ = self.tx.try_send("\r\n".to_owned());
                 let result = self.handle_cmd();
                 self.buffer.clear();
+                self.pos = 0;
                 let _ = self.tx.try_send(result);
                 let _ = self.tx.try_send(self.prompt());
             },
@@ -46,9 +73,13 @@ impl Shell {
                 self.buffer.pop();
                 let _ = self.tx.try_send("\u{0008} \u{0008}".to_owned());
             },
-            _ => {
-                self.buffer.push_str(key);
-                let _ = self.tx.try_send(key.to_owned());
+            _ => if key.chars().all(|c| !c.is_control()) {
+                self.buffer.insert_str(self.pos, key);
+                let output = format!("{}\x1B[K{}", key, &self.buffer[self.pos + key.len()..]);
+                let shift = self.buffer.len() - self.pos - key.len();
+                self.pos += key.len();
+                let out = if shift > 0 { format!("{}\x1B[{}D", output, shift) } else { output };
+                let _ = self.tx.try_send(out);
             },
         }
     }
