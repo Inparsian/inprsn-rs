7f71c64755fac7ea09643341761b082e1326a434
Inparsian <im@inpr.sn>
2026-08-10 13:58:23 -0400

    feat: add flags to rm

--- src/services/bins/rm.rs
+++ src/services/bins/rm.rs
@@ -1,5 +1,7 @@
+use std::fmt::Write as _;
+
 use super::{Command, CommandContext};
-use crate::services::fs::FILESYSTEM;
+use crate::services::fs::{FILESYSTEM, FilesystemData};
 
 pub struct Rm;
 pub const RM: Rm = Rm;
@@ -14,19 +16,81 @@ impl Command for Rm {
     }
 
     fn run(&self, ctx: &mut CommandContext, args: &[String]) -> String {
-        if args.is_empty() {
-            "rm: not enough arguments\r\n".to_owned()
-        } else {
-            // TODO: flags
-            let path = args.first().unwrap();
-            FILESYSTEM.write().unwrap().remove(path, Some(ctx.pwd)).map_or_else(
-                |err| format!("rm: {}\r\n", err),
-                |()| String::new(),
-            )
+        let mut recursive = false;
+        let mut force = false;
+        let mut paths: Vec<&String> = Vec::new();
+
+        let mut parsing_flags = true;
+        for arg in args {
+            if parsing_flags && arg == "--" {
+                parsing_flags = false;
+                continue;
+            }
+
+            if parsing_flags && arg.starts_with('-') && arg.len() > 1 {
+                for ch in arg.chars().skip(1) {
+                    match ch {
+                        'r' => recursive = true,
+                        'f' => force = true,
+                        _ => return format!("rm: invalid option -- '{}'\r\n", ch),
+                    }
+                }
+            } else {
+                paths.push(arg);
+            }
         }
+
+        if paths.is_empty() {
+            return if force {
+                String::new()
+            } else {
+                "rm: not enough arguments\r\n".to_owned()
+            };
+        }
+
+        let mut out = String::new();
+
+        for path in paths {
+            let (exists, is_dir) = {
+                let reader = FILESYSTEM.read().unwrap();
+                reader.resolve_read(path, Some(ctx.pwd)).map_or(
+                    (false, false),
+                    |entry| (true, matches!(entry.data, FilesystemData::Directory { .. }))
+                )
+            };
+
+            if !exists {
+                if !force {
+                    let _ = write!(out, "rm: cannot remove '{}': No such file or directory\r\n",
+                        path);
+                }
+                continue;
+            }
+
+            if is_dir && !recursive {
+                let _ = write!(out, "rm: cannot remove '{}': Is a directory\r\n", path);
+                continue;
+            }
+
+            if let Err(err) = FILESYSTEM.write().unwrap().remove(path, Some(ctx.pwd)) && !force {
+                let _ = write!(out, "rm: {}\r\n", err);
+            }
+        }
+
+        out
     }
 
     fn complete(&self, ctx: &mut CommandContext, args: &[String], _cursor: usize) -> Vec<String> {
-        super::complete_path(ctx, args, 0, false)
+        // Complete first non-flag token as path (keeps completion useful for: rm -r <tab>)
+        let path_index = args
+            .iter()
+            .position(|a| !a.starts_with('-') || a == "-")
+            .unwrap_or(args.len());
+
+        if path_index == args.len() {
+            return super::complete_path(ctx, &[String::new()], 0, false);
+        }
+
+        super::complete_path(ctx, args, path_index, false)
     }
 }
