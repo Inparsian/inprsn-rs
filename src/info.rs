09db00b8555f61d011c28577d8ef86b867b82dea
Inparsian <im@inpr.sn>
2026-08-14 20:23:51 -0400

    chore: add inprsn-rs to projects

--- src/info.rs
+++ src/info.rs
@@ -1,6 +1,7 @@
 pub const PROJECTS: &[(&str, &str, &str)] = &[
     ("gray-meadows-shell", "My personal stand-alone Hyprland desktop shell", "https://github.com/Inparsian/gray-meadows-shell"),
     ("gtk4cmark-rs", "A GTK4 widget for rendering CommonMark content", "https://github.com/Inparsian/gtk4cmark-rs"),
+    ("inprsn-rs", "This website!", "https://github.com/Inparsian/inprsn-rs"),
     ("SkinArtistWeb", "Automates the process of making NameMC skin art", "https://skinartistweb.inpr.sn"),
 ];
 
